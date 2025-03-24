1a8e6a1a537140bfcba90bef88e0e4098273cff2
Wesley Daniel <165509781+wesleydanieell@users.noreply.github.com>
2025-03-24 19:58:49 -0300

    Update and rename FacilitestApplication.java to Startup.java

--- src/main/java/br/com/facilitest/Startup.java
+++ src/main/java/br/com/facilitest/Startup.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FacilitestApplication {
+public class Startup {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FacilitestApplication.class, args);
