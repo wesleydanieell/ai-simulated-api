f08e5d3c816f1488b2e84b3f2ee911a875099a2f
Wesley <danielwesley266@gmail.com>
2025-03-24 19:55:46 -0300

    first steps of the project

---
+++ src/main/java/br/com/facilitest/FacilitestApplication.java
@@ -0,0 +1,13 @@
+package br.com.facilitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilitestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/facilitest/FacilitestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.facilitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilitestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
